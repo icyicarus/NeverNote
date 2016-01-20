55aa22b41e0a72a9da48a990747338afcdb63da6
IcyIcarus <xuxuenan888@126.com>
2016-01-20 18:48:49 +0800

    2016-01-20 18:45

--- app/src/main/java/bupt/icyicarus/nevernote/mediaList/MediaListCellData.java
+++ app/src/main/java/bupt/icyicarus/nevernote/mediaList/MediaListCellData.java
@@ -4,28 +4,26 @@ import bupt.icyicarus.nevernote.R;
 
 public class MediaListCellData {
 
-    public MediaListCellData(String path) {
-        this.path = path;
-
-        if (path.endsWith(".jpg")) {
-            iconID = R.drawable.imgphoto;
-            type = MediaType.PHOTO;
-        } else if (path.endsWith(".mp4")) {
-            iconID = R.drawable.imgvideo;
-            type = MediaType.VIDEO;
-        } else if (path.endsWith(".amr")) {
-            iconID = R.drawable.imgphoto;
-            type = MediaType.AUDIO;
-        }
-    }
-
-    public MediaListCellData(String path, int id) {
-        this(path);
-        this.id = id;
-    }
-
     public int type = 0;
     public int id = -1;
     public String path = null;
     public int iconID = R.drawable.logo;
+    public MediaListCellData(String path) {
+        this.path = path;
+
+        if (path.endsWith(".jpg")) {
+            iconID = R.drawable.img_photo;
+            type = MediaType.PHOTO;
+        } else if (path.endsWith(".mp4")) {
+            iconID = R.drawable.img_video;
+            type = MediaType.VIDEO;
+        } else if (path.endsWith(".amr")) {
+            iconID = R.drawable.img_photo;
+            type = MediaType.AUDIO;
+        }
+    }
+    public MediaListCellData(String path, int id) {
+        this(path);
+        this.id = id;
+    }
 }
