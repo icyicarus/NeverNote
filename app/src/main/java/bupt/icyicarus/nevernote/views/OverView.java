0efb166a7cc253e1c70c5688cffbd803c1835c80
Icarus <xuxuenan888@126.com>
2016-04-03 18:11:24 +0800

    2016-04-03 switch to card view

--- app/src/main/java/bupt/icyicarus/nevernote/views/OverView.java
+++ app/src/main/java/bupt/icyicarus/nevernote/views/OverView.java
@@ -1,6 +1,5 @@
 package bupt.icyicarus.nevernote.views;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -8,29 +7,36 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.AdapterView;
-import android.widget.ListView;
+
+import com.dexafree.materialList.card.Card;
+import com.dexafree.materialList.card.CardProvider;
+import com.dexafree.materialList.card.OnActionClickListener;
+import com.dexafree.materialList.card.action.TextViewAction;
+import com.dexafree.materialList.listeners.RecyclerItemClickListener;
+import com.dexafree.materialList.view.MaterialListView;
+import com.squareup.picasso.RequestCreator;
 
 import java.io.File;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 import bupt.icyicarus.nevernote.EditNote;
 import bupt.icyicarus.nevernote.R;
 import bupt.icyicarus.nevernote.db.NeverNoteDB;
 import bupt.icyicarus.nevernote.init.SetPortrait;
-import bupt.icyicarus.nevernote.noteList.NoteAdapter;
 import bupt.icyicarus.nevernote.noteList.NoteListCellData;
 
 public class OverView extends SetPortrait {
-    public static final int REQUEST_CODE_ADD_NOTE = 1;
-    public static final int REQUEST_CODE_EDIT_NOTE = 2;
-    private ListView mainListView;
-    private NoteAdapter adapter = null;
+    private MaterialListView materialListView;
+    private ArrayList<NoteListCellData> noteListCellDataArrayList = null;
     private NeverNoteDB db;
     private SQLiteDatabase dbRead, dbWrite;
     private long noteDate = -1;
@@ -44,82 +50,33 @@ public class OverView extends SetPortrait {
         Toolbar tbMain = (Toolbar) findViewById(R.id.tbOverView);
         setSupportActionBar(tbMain);
 
-        mainListView = (ListView) findViewById(R.id.mainListView);
+        materialListView = (MaterialListView) findViewById(R.id.materialListView);
 
         db = new NeverNoteDB(this);
         dbRead = db.getReadableDatabase();
         dbWrite = db.getWritableDatabase();
 
-        adapter = new NoteAdapter(this);
         noteDate = getIntent().getLongExtra("noteDate", -1);
-        refreshNotesListView();
 
-        mainListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                NoteListCellData data = adapter.getItem(position);
-                Intent i = new Intent(OverView.this, EditNote.class);
-
-                i.putExtra(EditNote.EXTRA_NOTE_ID, data.id);
-                i.putExtra(EditNote.EXTRA_NOTE_NAME, data.name);
-                i.putExtra(EditNote.EXTRA_NOTE_CONTENT, data.content);
-                startActivityForResult(i, REQUEST_CODE_EDIT_NOTE);
-            }
-        });
-        mainListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(final AdapterView<?> parent, View view, final int position, long id) {
-                new AlertDialog.Builder(OverView.this).setTitle("Delete?").setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        NoteListCellData data = adapter.getItem(position);
-                        File f;
-
-                        Cursor c = dbRead.query(NeverNoteDB.TABLE_NAME_MEDIA, null, NeverNoteDB.COLUMN_NAME_MEDIA_OWNER_NOTE_ID + "=?", new String[]{data.id + ""}, null, null, null);
-                        while (c.moveToNext()) {
-                            f = new File(c.getString(c.getColumnIndex(NeverNoteDB.COLUMN_NAME_MEDIA_PATH)));
-                            if (!f.delete())
-                                Log.e("file", "delete error");
-                        }
-                        dbWrite.delete(NeverNoteDB.TABLE_NAME_MEDIA, NeverNoteDB.COLUMN_NAME_MEDIA_OWNER_NOTE_ID + "=?", new String[]{data.id + ""});
-                        dbWrite.delete(NeverNoteDB.TABLE_NAME_NOTES, NeverNoteDB.COLUMN_ID + "=?", new String[]{data.id + ""});
-                        c.close();
-                        refreshNotesListView();
-                    }
-                }).setNegativeButton("No", null).show();
-                return true;
-            }
-        });
-
-        findViewById(R.id.fabAddNote).setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivityForResult(new Intent(OverView.this, EditNote.class), REQUEST_CODE_ADD_NOTE);
-            }
-        });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        switch (requestCode) {
-            case REQUEST_CODE_ADD_NOTE:
-            case REQUEST_CODE_EDIT_NOTE:
-                if (resultCode == Activity.RESULT_OK) {
-                    refreshNotesListView();
+        FloatingActionButton fabAddNote = (FloatingActionButton) findViewById(R.id.fabAddNote);
+        if (fabAddNote != null) {
+            fabAddNote.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(OverView.this, EditNote.class));
                 }
-                break;
-
-            default:
-                break;
+            });
         }
-
-        super.onActivityResult(requestCode, resultCode, data);
     }
 
-    public void refreshNotesListView() {
-        adapter.Clear();
-        Cursor c = dbRead.query(NeverNoteDB.TABLE_NAME_NOTES, null, null, null, null, null, null);
+    public void refreshNoteArrayList() {
+        materialListView.getAdapter().clearAll();
+        if (noteListCellDataArrayList != null) {
+            noteListCellDataArrayList.clear();
+        } else {
+            noteListCellDataArrayList = new ArrayList<>();
+        }
+        Cursor c = dbRead.query(NeverNoteDB.TABLE_NAME_NOTES, null, null, null, null, null, null, null);
         while (c.moveToNext()) {
             try {
                 noteAddDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(c.getString(c.getColumnIndex(NeverNoteDB.COLUMN_NAME_NOTE_DATE))).getTime();
@@ -128,16 +85,98 @@ public class OverView extends SetPortrait {
             }
 
             if (noteDate == -1 || (noteDate != -1 && noteAddDate >= noteDate && noteAddDate < (noteDate + 86400000))) {
-                adapter.Add(new NoteListCellData(c.getString(c.getColumnIndex(NeverNoteDB.COLUMN_NAME_NOTE_NAME)), c.getString(c.getColumnIndex(NeverNoteDB.COLUMN_NAME_NOTE_DATE)), c.getString(c.getColumnIndex(NeverNoteDB.COLUMN_NAME_NOTE_CONTENT)), c.getInt(c.getColumnIndex(NeverNoteDB.COLUMN_ID))));
+                noteListCellDataArrayList.add(new NoteListCellData(
+                        c.getString(c.getColumnIndex(NeverNoteDB.COLUMN_NAME_NOTE_NAME)),
+                        c.getString(c.getColumnIndex(NeverNoteDB.COLUMN_NAME_NOTE_DATE)),
+                        c.getString(c.getColumnIndex(NeverNoteDB.COLUMN_NAME_NOTE_CONTENT)),
+                        c.getInt(c.getColumnIndex(NeverNoteDB.COLUMN_ID))
+                ));
             }
         }
-        mainListView.setAdapter(adapter);
+        for (NoteListCellData noteListCellData : noteListCellDataArrayList) {
+            Card card = new Card.Builder(this)
+                    .setTag(noteListCellData)
+                    .withProvider(new CardProvider())
+                    .setLayout(R.layout.material_basic_image_buttons_card_layout)
+                    .setTitle(noteListCellData.name)
+                    .setTitleGravity(Gravity.END)
+                    .setDescription(noteListCellData.date)
+                    .setDescriptionGravity(Gravity.END)
+                    .setDrawable(R.drawable.img_audio)
+                    .setDrawableConfiguration(new CardProvider.OnImageConfigListener() {
+                        @Override
+                        public void onImageConfigure(@NonNull RequestCreator requestCreator) {
+                            requestCreator.fit();
+                        }
+                    })
+                    .addAction(R.id.left_text_button, new TextViewAction(this)
+                            .setText("Delete")
+                            .setTextResourceColor(R.color.black_button)
+                            .setListener(new OnActionClickListener() {
+                                @Override
+                                public void onActionClicked(View view, final Card card) {
+                                    new AlertDialog.Builder(OverView.this).setTitle("Delete this note?").setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            NoteListCellData data = (NoteListCellData) card.getTag();
+                                            File f;
+
+                                            Cursor c = dbRead.query(NeverNoteDB.TABLE_NAME_MEDIA, null, NeverNoteDB.COLUMN_NAME_MEDIA_OWNER_NOTE_ID + "=?", new String[]{data.id + ""}, null, null, null);
+                                            while (c.moveToNext()) {
+                                                f = new File(c.getString(c.getColumnIndex(NeverNoteDB.COLUMN_NAME_MEDIA_PATH)));
+                                                if (!f.delete())
+                                                    Log.e("file", "delete error");
+                                            }
+                                            dbWrite.delete(NeverNoteDB.TABLE_NAME_MEDIA, NeverNoteDB.COLUMN_NAME_MEDIA_OWNER_NOTE_ID + "=?", new String[]{data.id + ""});
+                                            dbWrite.delete(NeverNoteDB.TABLE_NAME_NOTES, NeverNoteDB.COLUMN_ID + "=?", new String[]{data.id + ""});
+                                            c.close();
+                                            refreshNoteArrayList();
+                                        }
+                                    }).setNegativeButton("No", null).show();
+                                }
+                            }))
+                    .addAction(R.id.right_text_button, new TextViewAction(this)
+                            .setText("Edit")
+                            .setTextResourceColor(R.color.orange_button)
+                            .setListener(new OnActionClickListener() {
+                                @Override
+                                public void onActionClicked(View view, Card card) {
+                                    NoteListCellData data = (NoteListCellData) card.getTag();
+                                    Intent i = new Intent(OverView.this, EditNote.class);
+
+                                    if (data != null) {
+                                        i.putExtra(EditNote.EXTRA_NOTE_ID, data.id);
+                                        i.putExtra(EditNote.EXTRA_NOTE_NAME, data.name);
+                                        i.putExtra(EditNote.EXTRA_NOTE_CONTENT, data.content);
+                                    }
+                                    startActivity(i);
+                                }
+                            }))
+                    .endConfig()
+                    .build();
+            materialListView.getAdapter().add(card);
+        }
+
+        if (materialListView != null) {
+            materialListView.addOnItemTouchListener(new RecyclerItemClickListener.OnItemClickListener() {
+                @Override
+                public void onItemClick(@NonNull Card card, int position) {
+                    Log.e("MaterialListView", "onItemClick");
+                }
+
+                @Override
+                public void onItemLongClick(@NonNull final Card card, int position) {
+                    Log.e("MaterialListView", "onItemLongClick");
+                }
+            });
+        }
         c.close();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        refreshNoteArrayList();
         if (customBackground) {
             findViewById(R.id.containerMainView).setBackgroundColor(Integer.parseInt(backgroundColor));
         } else {
