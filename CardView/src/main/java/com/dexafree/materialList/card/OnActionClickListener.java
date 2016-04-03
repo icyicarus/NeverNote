65a97cc4d6f91173223968e206acabc7b93972df
Icarus <xuxuenan888@126.com>
2016-04-03 18:16:19 +0800

    2016-04-03 switch to card view

---
+++ CardView/src/main/java/com/dexafree/materialList/card/OnActionClickListener.java
@@ -0,0 +1,16 @@
+package com.dexafree.materialList.card;
+
+import android.view.View;
+
+/**
+ * The OnActionClickListener will be notified if a Button is clicked on the Card.
+ */
+public interface OnActionClickListener {
+    /**
+     * An action view is clicked.
+     *
+     * @param view which was clicked.
+     * @param card where the view was clicked on.
+     */
+    void onActionClicked(View view, Card card);
+}
