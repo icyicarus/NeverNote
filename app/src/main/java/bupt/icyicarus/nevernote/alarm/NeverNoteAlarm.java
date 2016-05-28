1f1ad52812fb45e24be7cba2dbb1bba7059752e9
IcyIcarus <xuxuenan888@126.com>
2016-05-29 00:18:56 +0800

    2016-05-28 Change notification icon

--- app/src/main/java/bupt/icyicarus/nevernote/alarm/NeverNoteAlarm.java
+++ app/src/main/java/bupt/icyicarus/nevernote/alarm/NeverNoteAlarm.java
@@ -1,11 +1,12 @@
 package bupt.icyicarus.nevernote.alarm;
 
+import android.annotation.SuppressLint;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
-import android.graphics.BitmapFactory;
+import android.graphics.drawable.Icon;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -16,6 +17,7 @@ import bupt.icyicarus.nevernote.view.NoteView;
 
 public class NeverNoteAlarm extends AppCompatActivity {
 
+    @SuppressLint("NewApi")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,8 +28,8 @@ public class NeverNoteAlarm extends AppCompatActivity {
         i.setClass(NeverNoteAlarm.this, NoteView.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(NeverNoteAlarm.this, 0, i, PendingIntent.FLAG_UPDATE_CURRENT);
         builder.setContentIntent(pendingIntent)
-                .setSmallIcon(R.drawable.logo)
-                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(Icon.createWithResource(this, R.drawable.logo))
                 .setTicker("setTicker")
                 .setWhen(System.currentTimeMillis())
                 .setAutoCancel(true)
