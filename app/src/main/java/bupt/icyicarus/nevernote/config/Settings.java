fd124aa86326b2a87f950045c9179c59689e0c42
IcyIcarus <xuxuenan888@126.com>
2016-05-31 23:17:34 +0800

    2016-05-31 Use SwitchCompat

--- app/src/main/java/bupt/icyicarus/nevernote/config/Settings.java
+++ app/src/main/java/bupt/icyicarus/nevernote/config/Settings.java
@@ -1,12 +1,13 @@
 package bupt.icyicarus.nevernote.config;
 
 import android.os.Bundle;
+import android.support.v7.widget.SwitchCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.FrameLayout;
 
 import com.gc.materialdesign.views.ButtonRectangle;
-import com.gc.materialdesign.views.Switch;
 import com.gc.materialdesign.widgets.ColorSelector;
 
 import bupt.icyicarus.nevernote.R;
@@ -27,33 +28,33 @@ public class Settings extends Initialization {
     protected void onResume() {
         super.onResume();
 
-        Switch switchCustomBackground = (Switch) findViewById(R.id.switchCustomBackground);
-        switchCustomBackground.setChecked(customBackground);
+        SwitchCompat scShowOKButton = (SwitchCompat) findViewById(R.id.scShowOKButton);
+        scShowOKButton.setChecked(showOKButton);
 
-        Switch switchShowOKButton = (Switch) findViewById(R.id.switchShowOKButton);
-        switchShowOKButton.setChecked(showOKButton);
+        SwitchCompat scCustomBackground = (SwitchCompat) findViewById(R.id.scCustomBackground);
+        scCustomBackground.setChecked(customBackground);
 
         final FrameLayout containerBackgroundColor = (FrameLayout) findViewById(R.id.containerBackgroundColor);
         final ButtonRectangle sampleBackgroundColor = (ButtonRectangle) findViewById(R.id.sampleBackgroundColor);
         containerBackgroundColor.setVisibility(customBackground ? View.VISIBLE : View.GONE);
         sampleBackgroundColor.setBackgroundColor(Integer.parseInt(p.get("BACKGROUND_COLOR").toString()));
 
-        switchCustomBackground.setOncheckListener(new Switch.OnCheckListener() {
+        scShowOKButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onCheck(Switch view, boolean check) {
-                customBackground = check;
-                containerBackgroundColor.setVisibility(customBackground ? View.VISIBLE : View.GONE);
-                sampleBackgroundColor.setBackgroundColor(Integer.parseInt(backgroundColor));
-                p.put("CUSTOM_BACKGROUND", check + "");
-                p.put("BACKGROUND_COLOR", backgroundColor);
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                showOKButton = isChecked;
+                p.put("SHOW_OK", isChecked + "");
             }
         });
 
-        switchShowOKButton.setOncheckListener(new Switch.OnCheckListener() {
+        scCustomBackground.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onCheck(Switch view, boolean check) {
-                showOKButton = check;
-                p.put("SHOW_OK", check + "");
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                customBackground = isChecked;
+                containerBackgroundColor.setVisibility(customBackground ? View.VISIBLE : View.GONE);
+                sampleBackgroundColor.setBackgroundColor(Integer.parseInt(backgroundColor));
+                p.put("CUSTOM_BACKGROUND", isChecked + "");
+                p.put("BACKGROUND_COLOR", backgroundColor);
             }
         });
 
